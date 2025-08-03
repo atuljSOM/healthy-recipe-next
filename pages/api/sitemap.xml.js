990fad40a98c3d3d1f85c69974067f45c304491d
atuljSOM <1010.atul@gmail.com>
2025-08-02 20:35:42 -0700

    Update sitemap.xml.js

--- pages/api/sitemap.xml.js
+++ pages/api/sitemap.xml.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/xml");
+  res.setHeader('Content-Type', 'application/xml');
   res.status(200).end(`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -9,3 +9,9 @@ export default function handler(req, res) {
   </url>
 </urlset>`);
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
