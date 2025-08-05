e38fec8964489ef389a38b0c532250fc8340c4d3
atuljSOM <1010.atul@gmail.com>
2025-08-04 22:46:28 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -39,7 +39,11 @@ export default function HealthyRecipe() {
         <meta property="og:image:height" content="630" />
 
       
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       
         {recipe && (
           <script
