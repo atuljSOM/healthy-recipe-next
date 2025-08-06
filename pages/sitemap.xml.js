f524c8516cc8f4b34cf48b105e73b96bfacc3aa8
atuljSOM <1010.atul@gmail.com>
2025-08-06 09:12:55 -0700

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,33 +1,20 @@
+// pages/sitemap.xml.js
 export async function getServerSideProps({ res }) {
-  const baseUrl = 'https://dailyhealthyrecipe.com';
-
-  const staticPages = [
-    '',
-    'about',
-    'contact',
-    'privacy',
-    'terms'
-  ];
-
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${staticPages
-      .map(
-        (page) => `
-      <url>
-        <loc>${baseUrl}/${page}</loc>
-        <changefreq>monthly</changefreq>
-        <priority>${page === '' ? '1.0' : '0.5'}</priority>
-      </url>`
-      )
-      .join('')}
-  </urlset>`;
-
   res.setHeader('Content-Type', 'application/xml');
-  res.write(sitemap);
+  res.write(`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://dailyhealthyrecipe.com/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+      </url>
+    </urlset>
+  `);
   res.end();
 
-  return { props: {} };
+  return {
+    props: {},
+  };
 }
 
 export default function Sitemap() {
