01ee797b96ea7e7030694cd7804102e30bb04660
atuljSOM <1010.atul@gmail.com>
2025-08-06 09:01:22 -0700

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,6 +1,6 @@
-export default function handler(req, res) {
+export async function getServerSideProps({ res }) {
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).end(`<?xml version="1.0" encoding="UTF-8"?>
+  res.write(`<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>https://dailyhealthyrecipe.com/</loc>
@@ -8,4 +8,11 @@ export default function handler(req, res) {
       <priority>1.0</priority>
     </url>
   </urlset>`);
+  res.end();
+
+  return { props: {} };
+}
+
+export default function Sitemap() {
+  return null;
 }
