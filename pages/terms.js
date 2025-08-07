100b142277c9444dc93c1eb1212cfd426cd1079d
atuljSOM <1010.atul@gmail.com>
2025-08-07 09:25:59 -0700

    Update terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -9,6 +9,11 @@ export default function TermsPage() {
         <title>Terms of Service | Daily Healthy Recipe</title>
         <meta name="description" content="Terms of use for visitors and subscribers of Daily Healthy Recipe." />
         <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+
+
       </Head>
 
       <main className="min-h-screen bg-white text-gray-800 px-6 py-12">
