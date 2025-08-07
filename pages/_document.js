4a6d204d7f7a9ec48c6309c55999dc5828fec5f0
atuljSOM <1010.atul@gmail.com>
2025-08-06 21:47:47 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,16 @@
 // pages/_document.js
-
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+
         {/* Google Analytics */}
         <script
           async
