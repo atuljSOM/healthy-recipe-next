d5347a083fac494b2a9a4c6b03a3307cc8312292
atuljSOM <1010.atul@gmail.com>
2025-08-07 09:25:37 -0700

    Update privacy.js

--- pages/privacy.js
+++ pages/privacy.js
@@ -9,6 +9,10 @@ export default function PrivacyPolicy() {
         <title>Privacy Policy | Daily Healthy Recipe</title>
         <meta name="description" content="Our privacy policy explains how we collect and protect your data when using Daily Healthy Recipe." />
         <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+
 
       </Head>
 
